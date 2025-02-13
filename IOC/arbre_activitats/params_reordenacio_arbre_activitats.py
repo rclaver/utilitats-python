f1a84184329ffd245a2ea0c06ca9f03ef6028ba7
rclaver <rclaver@xtec.cat>
2025-02-13 13:20:51 +0100

    reubicació mòduls IOC

--- IOC/arbre_activitats/params_reordenacio_arbre_activitats.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: UTF8 -*-
-mdprojects="/home/rafael/Vídeos/mdprojects/activitats"
-pages="/home/rafael/Vídeos/pages/activitats"
-activity_dir="/home/rafael/Vídeos/ActivityUtil"
-prefix="documents_fp_docencia_activitats_"
-simulació="S"
-
