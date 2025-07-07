b30c77e637dc83bfde3f0e681aac0a0668275701
rclaver <rclaver@xtec.cat>
2025-07-07 17:27:24 +0200

    neteja

--- IOC/arbre/var.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: UTF8 -*-
-mdprojects="/home/rafael/projectes/data/mdprojects/activitats"
-activity_dir="/home/rafael/projectes/data/ActivityUtil"
-activity_base="documents_fp_docencia_activitats_"
